b310c402448f3cde29b5a98f24851cd60ee9a224
Alexey Gruzdev <alex.gruzdev@hotmail.com>
2026-02-24 20:24:50 +0300

    Updated zlib v1.3.1 -> v1.3.2

--- cmake/dependency.zlib.cmake
+++ cmake/dependency.zlib.cmake
@@ -12,8 +12,8 @@ include(${EXTERNALPROJECT_INCLUDE_DIR}/external_project_common.cmake)
 
 ExternalProject_Add(ZLIB
     PREFIX "${EXTERNALPROJECT_BINARY_ROOT}/zlib"
-    URL "https://github.com/madler/zlib/archive/refs/tags/v1.3.1.zip"
-    URL_MD5 "127b8a71a3fb8bebe89df1080f15fdf6"
+    URL "https://github.com/madler/zlib/archive/refs/tags/v1.3.2.zip"
+    URL_MD5 "adbba6eef8960c3412818b2e241f46dc"
     DOWNLOAD_DIR "${EXTERNALPROJECT_SOURCE_ROOT}/zlib"
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     SOURCE_DIR "${EXTERNALPROJECT_SOURCE_PREFIX}/zlib/source"
@@ -22,8 +22,8 @@ ExternalProject_Add(ZLIB
     UPDATE_COMMAND ""
     INSTALL_COMMAND ${BUILD_COMMAND_FOR_TARGET} -t install
             COMMAND ${CMAKE_COMMAND} -E copy "${EXTERNALPROJECT_SOURCE_PREFIX}/zlib/source/zutil.h" -t "${EXTERNALPROJECT_BINARY_ROOT}/zlib/install/include"
-    CMAKE_ARGS ${CMAKE_TOOLCHAIN_FILE_ARG} ${CMAKE_BUILD_TYPE_ARG}
-        "-DCMAKE_INSTALL_PREFIX:PATH=${EXTERNALPROJECT_BINARY_ROOT}/zlib/install" "-DZLIB_BUILD_EXAMPLES=OFF"
+    CMAKE_ARGS ${CMAKE_TOOLCHAIN_FILE_ARG} ${CMAKE_BUILD_TYPE_ARG} "-DCMAKE_INSTALL_PREFIX:PATH=${EXTERNALPROJECT_BINARY_ROOT}/zlib/install"
+        "-DZLIB_BUILD_TESTING=OFF" "-DZLIB_BUILD_SHARED=OFF" "-DZLIB_BUILD_STATIC=ON"
         "-DCMAKE_C_FLAGS:STRING=${ZERO_WARNINGS_FLAG} ${FPIC_FLAG}"
 )
 
@@ -35,11 +35,11 @@ unset(INSTALL_DIR)
 add_library(LibZLIB INTERFACE)
 add_dependencies(LibZLIB ZLIB)
 target_include_directories(LibZLIB INTERFACE ${ZLIB_ROOT}/include)
-if(WIN32 AND NOT ANDROID)
-    if (MSVC AND IS_DEBUG_CONFIG)
-        target_link_libraries(LibZLIB INTERFACE ${ZLIB_ROOT}/lib/zlibstaticd${CMAKE_STATIC_LIBRARY_SUFFIX})
+if(WIN32)
+    if (IS_DEBUG_CONFIG)
+        target_link_libraries(LibZLIB INTERFACE ${ZLIB_ROOT}/lib/zsd${CMAKE_STATIC_LIBRARY_SUFFIX})
     else()
-        target_link_libraries(LibZLIB INTERFACE ${ZLIB_ROOT}/lib/zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX})
+        target_link_libraries(LibZLIB INTERFACE ${ZLIB_ROOT}/lib/zs${CMAKE_STATIC_LIBRARY_SUFFIX})
     endif()
 else()
     target_link_libraries(LibZLIB INTERFACE ${ZLIB_ROOT}/lib/libz${CMAKE_STATIC_LIBRARY_SUFFIX})
