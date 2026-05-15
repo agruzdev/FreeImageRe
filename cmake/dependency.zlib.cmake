8effb3ef57e05ba3ebb18c90cb2fe5c1dd35488d
Lior Lahav <LahavLior@gmail.com>
2026-05-15 09:13:43 +0300

    Declare zlib install byproduct
    The zlib external project links FreeImage against the installed static library, but Ninja needs that generated file declared on the install step. Record the platform-specific zlib library name and expose it through INSTALL_BYPRODUCTS while still deriving ZLIB_ROOT from the ExternalProject install directory.

--- cmake/dependency.zlib.cmake
+++ cmake/dependency.zlib.cmake
@@ -9,6 +9,15 @@ set(_ZLIB_DEP_INCLUDE_GUARD_ ON)
 
 include(${EXTERNALPROJECT_INCLUDE_DIR}/external_project_common.cmake)
 
+if(WIN32)
+    if (IS_DEBUG_CONFIG)
+        set(ZLIB_LIBRARY_NAME zsd${CMAKE_STATIC_LIBRARY_SUFFIX})
+    else()
+        set(ZLIB_LIBRARY_NAME zs${CMAKE_STATIC_LIBRARY_SUFFIX})
+    endif()
+else()
+    set(ZLIB_LIBRARY_NAME libz${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
 
 ExternalProject_Add(ZLIB
     PREFIX "${EXTERNALPROJECT_BINARY_ROOT}/zlib"
@@ -22,6 +31,7 @@ ExternalProject_Add(ZLIB
     UPDATE_COMMAND ""
     INSTALL_COMMAND ${BUILD_COMMAND_FOR_TARGET} -t install
             COMMAND ${CMAKE_COMMAND} -E copy "${EXTERNALPROJECT_SOURCE_PREFIX}/zlib/source/zutil.h" -t "${EXTERNALPROJECT_BINARY_ROOT}/zlib/install/include"
+    INSTALL_BYPRODUCTS "<INSTALL_DIR>/lib/${ZLIB_LIBRARY_NAME}"
     CMAKE_ARGS ${CMAKE_TOOLCHAIN_FILE_ARG} ${CMAKE_BUILD_TYPE_ARG} "-DCMAKE_INSTALL_PREFIX:PATH=${EXTERNALPROJECT_BINARY_ROOT}/zlib/install"
         "-DZLIB_BUILD_TESTING=OFF" "-DZLIB_BUILD_SHARED=OFF" "-DZLIB_BUILD_STATIC=ON"
         "-DCMAKE_C_FLAGS:STRING=${ZERO_WARNINGS_FLAG} ${FPIC_FLAG}"
@@ -30,17 +40,8 @@ ExternalProject_Add(ZLIB
 # For configuring other dependencies
 ExternalProject_Get_Property(ZLIB INSTALL_DIR)
 set(ZLIB_ROOT ${INSTALL_DIR})
+set(ZLIB_LIBRARY ${ZLIB_ROOT}/lib/${ZLIB_LIBRARY_NAME})
 unset(INSTALL_DIR)
-
-if(WIN32)
-    if (IS_DEBUG_CONFIG)
-        set(ZLIB_LIBRARY ${ZLIB_ROOT}/lib/zsd${CMAKE_STATIC_LIBRARY_SUFFIX})
-    else()
-        set(ZLIB_LIBRARY ${ZLIB_ROOT}/lib/zs${CMAKE_STATIC_LIBRARY_SUFFIX})
-    endif()
-else()
-    set(ZLIB_LIBRARY ${ZLIB_ROOT}/lib/libz${CMAKE_STATIC_LIBRARY_SUFFIX})
-endif()
 
 add_library(LibZLIB INTERFACE)
 add_dependencies(LibZLIB ZLIB)
